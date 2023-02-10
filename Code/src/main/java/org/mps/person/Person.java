4a864ccaea8a57c1e0aee84307ea43e0cbd5da3f
Valenciaal <valeaal@uma.es>
2023-02-10 14:24:26 +0100

    Creación de Person.java

---
+++ Code/src/main/java/org/mps/person/Person.java
@@ -0,0 +1,47 @@
+package org.mps.person;
+
+
+import java.util.*;
+
+public class Person {
+
+    private final String name;
+    private final int age;
+    private final String gender; //Male, Female
+
+    public Person (String n, int a, String g){
+        name = n;
+        age = a;
+        gender = g;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public double [] averageAgePerGender(List<Person> lista){
+        int hombres = 0;
+        int mujeres = 0;
+        int edadhombres = 0;
+        int edadmujeres = 0;
+        for (Person act : lista) {
+            if(act.gender.equals("Male")){
+                hombres++;
+                edadhombres += act.age;
+            } else{
+                mujeres++;
+                edadmujeres += act.age;
+            }
+        }
+        double res[] = {edadhombres/hombres, edadmujeres/mujeres};
+        return res;
+    }
+}
